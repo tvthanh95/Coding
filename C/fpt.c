b0ca10cc4691f5acaf64c42a014dcf6e518f87cf
tvthanh95 <truongvanthanh95@hotmail.com>
2016-05-16 18:58:55 +0200

    using nano

--- C/fpt.c
+++ C/fpt.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
-struct complex
+struct Complex
 {
 	int im;
 	int re;
 };
-struct complex make_complex(int im, int re)
+struct Complex make_Complex(int im, int re)
 {
-	struct complex temp;
+	struct Complex temp;
 	temp.im = im;
 	temp.re = re;
 	return temp;
 }
-struct complex add(struct complex x, struct complex y)
+struct Complex add(struct Complex x, struct Complex y)
 {
-	struct complex temp;
+	struct Complex temp;
 	temp.im = x.im + y.im;
 	temp.re = x.re + y.re;
 	return temp;
 }
-void print_complex(struct complex x)
+void print_Complex(struct Complex x)
 {
 	printf("%d + %di\n",x.im, x.re);
 }
 int main()
 {
-	struct complex (*ptr1)(int, int);
-	ptr1 = make_complex;
-	struct complex c = ptr1(2,5);
-	struct complex d = ptr1(3,6);
-	struct complex (*ptr2)(struct complex, struct complex);
+	struct Complex (*ptr1)(int, int);
+	ptr1 = make_Complex;
+	struct Complex c = ptr1(2,5);
+	struct Complex d = ptr1(3,6);
+	struct Complex (*ptr2)(struct Complex, struct Complex);
 	ptr2 = add;
-	//print_complex(ptr2(c, d));
-	struct complex (*ptr[2])(int, int);
-	ptr[0] = make_complex;
-	ptr[1] = make_complex;
-	print_complex(ptr2(ptr[0](2,0), ptr[1](-2,0)));
+	//print_Complex(ptr2(c, d));
+	struct Complex (*ptr[2])(int, int);
+	ptr[0] = make_Complex;
+	ptr[1] = make_Complex;
+	print_Complex(ptr2(ptr[0](2,0), ptr[1](-2,0)));
 	return 0;
 }
